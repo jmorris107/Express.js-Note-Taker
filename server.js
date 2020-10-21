da7f24be7b6f6f8f23ee8c537dd9a973e9fa997c
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2020-10-21 18:33:20 -0400

    initial commit Heroku

---
+++ server.js
@@ -0,0 +1,53 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var fs = require("fs");
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+// app.get("/", function(req, res) {
+//     response.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
+
+//   app.post("/api/notes", function(req, res) {
+//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+//     // It will do this by sending out the value "true" have a table
+//     // req.body is available since we're using the body parsing middleware
+//       notes.push(req.body);
+//       res.json(true)
+// });
